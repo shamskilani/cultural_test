5acfe7f7c9b8c5a47ace850260c277be97fa6f24
Ebrahim Shamskilani <e.shams@tci.ir>
2023-06-23 09:38:22 +0000

    delete cultural test

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
       resources :culture_types, only: [:create, :index]
       resources :companies, only: [:create, :index]
       resources :applicants, only: [:create, :index]
-      resources :culture_tests, only: [:create]
     end
   end
 end
